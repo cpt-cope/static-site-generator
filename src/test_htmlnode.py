1c7ce659a16ce0191a3a05cd0650c76630817886
cpt-cope <copeandsongaming@gmail.com>
2026-05-18 22:37:02 -0500

    monday

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 test_prop = {"href": "https://www.google.com", "target": "_blank"}
 test_prop2 = {"href": "https://www.google.com"}
@@ -20,6 +20,17 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode("a", "two props", None, test_prop)
         self.assertEqual(node.__repr__(), "HTMLNode(a, two props, None, {'href': 'https://www.google.com', 'target': '_blank'})")
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node1 = LeafNode("p", "This is a paragraph of text.")
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node1.to_html(), "<p>This is a paragraph of text.</p>")
+        self.assertEqual(node2.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_repr(self):
+        node1 = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node1.__repr__(), "LeafNode(p, This is a paragraph of text., None)")
 
 if __name__ == "__main__":
     unittest.main()
